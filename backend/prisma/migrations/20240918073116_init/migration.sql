743c8ff6d45d17bdd59602816f93d5a32697634d
Nikunj <mnikunj262@gmail.com>
2024-09-18 13:35:32 +0530

    delete

--- backend/prisma/migrations/20240918073116_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Order" (
-    "id" SERIAL NOT NULL,
-    "customerName" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-
-    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
-);
